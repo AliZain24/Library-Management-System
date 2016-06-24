82bff93f9657ebaf0269ce4609dc2db7fa3bb0b4
AliZain24 <bsee14024@itu.edu.pk>
2016-06-24 07:32:59 -0700

    Update Final Project.cpp

--- Final Project.cpp
+++ Final Project.cpp
@@ -30,6 +30,16 @@ void book::settername(string name)
 	features['n']=name;
 }
 
+void book::setterid(string id)
+{
+	features['i']=id;
+}
+
+string book::getterid()
+{
+	return features['i'];
+}
+
 class library
 {
 	private:
@@ -46,69 +56,19 @@ void library::setterbook(char index,book obj)
 		collection[index]=obj;
 }
 
-class person
-{
-	private:
-		map<char,string> info;
-	
-	public:	
-		void settername(string name);
-		string gettername();
-		void setterid(string id);
-		string getterid();
-		
-	protected:
-		
-};
 
-void person::settername(string name)
-{
-	info['n']=name;
-}
-
-string person::gettername()
-{
-	return info['n'];
-}
-
-string person::getterid()
-{
-	return info['i'];
-}
-
-void person::setterid(string id)
-{
-	info['i']=id;
-}
-
-class librarian:public person
-{
-	
-};
-
-class member:public person
-{
-	public:
-		void leavemembership(string id, string name);
-		void returnbook(book obj,int returndate);
-		void issuebook(book obj,int issuedate);
-};
 
 int main(void)
 {
 	book obj;
-	string name;
+	string name,id;
 	cout<<"Please enter the name of the book:";
 	cin>>name;
 	obj.settername(name);
-	cout<<endl<<obj.gettername()<<endl;
-	librarian obj1;
-	obj1.setterid("123456");
-	obj1.settername("admin");
-	cout<<"Librarian created..........."<<endl;
-	cout<<"ID: "<<obj1.getterid()<<endl;
-	cout<<"Name: "<<obj1.gettername()<<endl;
-	ofstream outputfile("Library.txt");
-	outputfile<<obj1.gettername()<<endl;
-	outputfile<<"Alizain";
+	cout<<"Please enter the id of the book:";
+	cin>>id;
+	obj.setterid(id);
+	ofstream outputfile("Librarybooks.txt");
+	outputfile<<obj.gettername()<<endl;
+	outputfile<<obj.getterid()<<endl;
 }
