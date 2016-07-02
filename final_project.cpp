d79086ae40cfae38a78b6e60f26efc5e718f3c0d
msarfraz100786 <msarfraz100786@gmail.com>
2016-07-02 05:25:46 -0700

    create admin id and set password

---
+++ final_project.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<string.h>
+#include<map>
+using namespace std;
+
+int main()
+{
+	string check_password;
+	string password="pata ni";
+	cout<<"ID:			Admin"<<endl;
+	cout<<"Password		";
+	cin>>check_password;
+	
+}
