a74db56fefb273bfcfac8dc35693509aba236608
msarfraz100786 <msarfraz100786@gmail.com>
2016-07-04 11:22:27 +0500

    main function

---
+++ final project.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	void main();
+}
+
+void main()
+{
+	cout<<"***************Library Management System*******************"<<endl<<endl;
+ 	cout<<"What do you want to do?"<<endl;
+ 	cout<<"To add a book, enter 1"<<endl;
+ 	cout<<"To remove a book,enter 2"<<endl;
+ 	cout<<"To view all books, enter 3"<<endl;
+ 	cout<<"To find a book, enter 4"<<endl;
+ 	cout<<"To edit a book, enter 5"<<endl;
+ 	cout<<"To add a user, enter 6"<<endl;
+ 	cout<<"To remove a user, enter 7"<<endl;
+	 cout<<"To exit, enter 0"<<endl;
+ 	cout<<"Your choice:";
+}
