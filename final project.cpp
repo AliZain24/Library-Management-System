e6cc5e6affb851a6863e689cbaf343de2137dc9e
msarfraz100786 <msarfraz100786@gmail.com>
2016-07-04 11:36:43 +0500

    change in main function

--- final project.cpp
+++ final project.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 using namespace std;
-
+void Main();
 int main(void)
 {
-	void main();
+	 Main();
 }
 
-void main()
+void Main()
 {
 	cout<<"***************Library Management System*******************"<<endl<<endl;
  	cout<<"What do you want to do?"<<endl;
- 	cout<<"To add a book, enter 1"<<endl;
- 	cout<<"To remove a book,enter 2"<<endl;
- 	cout<<"To view all books, enter 3"<<endl;
- 	cout<<"To find a book, enter 4"<<endl;
- 	cout<<"To edit a book, enter 5"<<endl;
- 	cout<<"To add a user, enter 6"<<endl;
- 	cout<<"To remove a user, enter 7"<<endl;
-	 cout<<"To exit, enter 0"<<endl;
+ 	cout<<"\tTo add a book, enter 1"<<endl;
+ 	cout<<"\tTo remove a book,enter 2"<<endl;
+ 	cout<<"\tTo view all books, enter 3"<<endl;
+ 	cout<<"\tTo find a book, enter 4"<<endl;
+ 	cout<<"\tTo edit a book, enter 5"<<endl;
+ 	cout<<"\tTo add a user, enter 6"<<endl;
+ 	cout<<"\tTo remove a user, enter 7"<<endl;
+	 cout<<"\tTo exit, enter 0"<<endl;
  	cout<<"Your choice:";
 }
+
+
+
